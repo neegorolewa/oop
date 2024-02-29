da235bea9546062d97f6bf85f46cc7516a6ff23c
Eugene <cullofdute19762005@gmail.com>
2024-03-01 01:21:15 +0300

    программа готова, осталось дописать тесты на исключения

--- Lw1/invert/invert/invert.cpp
+++ Lw1/invert/invert/invert.cpp
@@ -3,9 +3,13 @@
 #include <string>
 #include <array>
 #include <sstream>
+#include <algorithm> 
+#include <cmath>
 
 const int SIZE_OF_MATRIX = 3;
 const char SPACE = ' ';
+const int NEGATIVE_SIGN = -1;
+const int NEGATIVE_ZERO = -0;
 
 bool CheckValidArguments(const int argc)
 {
@@ -112,7 +116,91 @@ void TranspositionOfMatrix(std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF
 	{
 		for (int j = 0; j < SIZE_OF_MATRIX; j++)
 		{
-			if (i != j)
+			if (i != j && i < j)
+			{
+				std::swap(matrix[i][j], matrix[j][i]);
+			}
+		}
+	}
+}
+
+float FindMinor(std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX>& matrix, int row, int column)
+{
+	int count = 0;
+
+	float values[4];
+
+	for (int i = 0; i < SIZE_OF_MATRIX; i++)
+	{
+		if (i == row)
+		{
+			continue;
+		}
+		for (int j = 0; j < SIZE_OF_MATRIX; j++)
+		{
+			if (j != column)
+			{
+				values[count++] = matrix[i][j];
+			}
+		}
+	}
+
+	return values[0] * values[3] - values[1] * values[2];
+}
+
+std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX> GetMatrixOfMinor(std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX>& matrix)
+{
+	std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX> MatrixOfMinor;
+
+	for (int i = 0; i < SIZE_OF_MATRIX; i++)
+	{
+		for (int j = 0; j < SIZE_OF_MATRIX; j++)
+		{
+			if ((i + j) % 2 == 0)
+			{
+				MatrixOfMinor[i][j] = FindMinor(matrix, i, j);
+			}
+			else
+			{
+				MatrixOfMinor[i][j] = FindMinor(matrix, i, j) * NEGATIVE_SIGN;
+			}
+		}
+	}
+
+	return MatrixOfMinor;
+}
+
+float RoundValue(float& value)
+{
+	return round(value * 1000) / 1000;
+}
+
+void InvertMatrix(std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX>& matrix, const float& det)
+{
+	matrix = GetMatrixOfMinor(matrix);
+
+	TranspositionOfMatrix(matrix);
+
+	for (int i = 0; i < SIZE_OF_MATRIX; i++)
+	{
+		for (int j = 0; j < SIZE_OF_MATRIX; j++)
+		{
+			matrix[i][j] = matrix[i][j] / det;
+			matrix[i][j] = RoundValue(matrix[i][j]);
+		}
+	}
+}
+
+void ChangeNegativeZero(std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX>& matrix)
+{
+	for (int i = 0; i < SIZE_OF_MATRIX; i++)
+	{
+		for (int j = 0; j < SIZE_OF_MATRIX; j++)
+		{
+			if (matrix[i][j] == NEGATIVE_ZERO)
+			{
+				matrix[i][j] = 0;
+			}
 		}
 	}
 }
@@ -127,10 +215,31 @@ int main(int argc, char* argv[])
 	std::ifstream inputFile = GetInputFile(argv[1]);
 
 	std::array<std::array<float, SIZE_OF_MATRIX>, SIZE_OF_MATRIX> matrix;
+	float determinant;
 
-	ReadMatrixFromFile(inputFile, matrix);
+	if (!ReadMatrixFromFile(inputFile, matrix))
+	{
+		std::cout << "Failed to read input file\n";
+		std::cout << "Input file consist of float number and size of matrix must be 3*3\n";
+		std::cout << "Сheck if the input file is empty\n";
+		return 1;
+	}
+
+	determinant = DefinitionOfDeterminant(matrix);
+	if (determinant != 0)
+	{
+		determinant = RoundValue(determinant);
+		InvertMatrix(matrix, determinant);
+	}
+	else
+	{
+		std::cout << "Determinant is 0, inverse matrix does not exist\n";
+		return 0;
+	}
+
+	ChangeNegativeZero(matrix);
+
 	PrintMatrix(matrix);
-	std::cout << '\n' << DefinitionOfDeterminant(matrix);
 
 	return 0;
 }
--- Lw1/invert/invert/tests.bat
+++ Lw1/invert/invert/tests.bat
@@ -6,9 +6,30 @@ if %MyProgram%=="" (
 	exit /B 1
 )
 
+REM "$(ProjectDir)tests.bat" "$(TargetPath)"
+
 REM If input file is not specified, program must be fail
 %MyProgram% && goto err
 echo Test 1 passed
+
+REM Is determinante of matrix is 0
+%MyProgram% input_zero_det.txt > output.txt || goto err
+fc output.txt message_zero_det.txt > nul || goto err
+echo Test 2 passed
+
+REM If input file is empty, program must be fail
+%MyProgram% empty.txt > output.txt && goto err
+echo Test 3 passed
+
+REM Test input1.txt, determinante must be 0 
+%MyProgram% input1.txt > output.txt || goto err
+fc output.txt message_zero_det.txt > nul || goto err
+echo Test 4 passed
+
+REM Test input2.txt 
+%MyProgram% input3.txt > output.txt || goto err
+fc output.txt output3.txt > nul || goto err
+echo Test 5 passed
 
 echo All test passed succesfuly
 exit /B 0
